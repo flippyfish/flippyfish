ae790342d36d8c50c059f967c703baadb19073d0
Jonathan Brown <brownjona1@ecs.vuw.ac.nz>
2018-08-17 10:32:36 +1200

    Closes #3: Justin made all the assets prefabs to lay the groundwork for multiple levels

---
+++ Assets/Scripts/Ragdoll.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+
+    internal Rigidbody RBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.gameObject.AddComponent<Rigidbody>();
+        this.RBody = this.gameObject.GetComponent<Rigidbody>();
+        //this.RBody.isKinematic = false;
+
+        int childCount = this.transform.childCount;
+
+        for (int i = 0; i < childCount; i++)
+        {
+            Transform t = this.transform.GetChild(i);
+
+            t.gameObject.AddComponent<HingeJoint>();
+
+            HingeJoint hinge = t.gameObject.GetComponent<HingeJoint>();
+
+            hinge.connectedBody =
+                i == 0 ? this.RBody :
+                this.transform.GetChild(i - 1).GetComponent<Rigidbody>();
+
+            hinge.useSpring = true;
+            hinge.enableCollision = true;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/SpawnHazards.cs
+++ Assets/Scripts/SpawnHazards.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnHazards : MonoBehaviour {
 
 	public GameObject car;
+    public GameObject car2;
 	public float carInterval;
 
 	// Use this for initialization
@@ -15,13 +16,21 @@ public class SpawnHazards : MonoBehaviour {
 
 	IEnumerator SpawnCars()
 	{
-		while (true)
-		{
-            Vector3 spawnPosition = new Vector3 (-40.0f, 1.644637f, 22.0f);
-			Quaternion spawnRotation = Quaternion.LookRotation(new Vector3(1.0f, 0.0f, 0.0f));
-			Instantiate (car, spawnPosition, spawnRotation);
-			yield return new WaitForSeconds(carInterval);
-		}
+        while (true)
+        {
+            // spawn car 1
+            Vector3 spawnPosition = new Vector3(-40.0f, 1.644637f, 22.0f);
+            Quaternion spawnRotation = Quaternion.LookRotation(new Vector3(1.0f, 0.0f, 0.0f));
+            Instantiate(car, spawnPosition, spawnRotation);
+
+            // spawn car 2
+            Vector3 spawnPosition2 = new Vector3(30.42976f, 1.5f, 22.0f);
+            Quaternion spawnRotation2 = Quaternion.LookRotation(new Vector3(1.0f, 0.0f, 0.0f));
+            Instantiate(car2, spawnPosition2, spawnRotation2);
+
+            // time between cars
+            yield return new WaitForSeconds(carInterval);
+        }
 	}
 	
 	// Update is called once per frame
