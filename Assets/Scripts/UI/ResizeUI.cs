42cf7b86fc87ac20133cbffbe760b843a9306de4
VICTOR <parkchan4@ecs.vuw.ac.nz>
2018-09-25 23:34:36 +1200

    Commit first UI resizing work. Everything has to be reworked since any of them has been set up for Responsive design. DO NOT USE ResizeUI script. It requires more works.

---
+++ Assets/Scripts/UI/ResizeUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResizeUI : MonoBehaviour {
+
+    public float ratioWidth;
+    public float ratioHeight;
+    Text m_Text;
+    RectTransform m_RectTransform;
+    // Use this for initialization
+    void Start () {
+        //Fetch the Text and RectTransform components from the GameObject
+        m_Text = GetComponent<Text>();
+        //m_RectTransform = GetComponent<RectTransform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+        int newFontSize = (int)(screenWidth /10);
+        //screenWidth = screenWidth * ratioWidth;
+        //screenHeight = screenHeight * ratioHeight;
+        m_Text.fontSize = newFontSize;
+        //m_RectTransform.sizeDelta = new Vector3(screenWidth, screenHeight, 0);
+    }
+}
