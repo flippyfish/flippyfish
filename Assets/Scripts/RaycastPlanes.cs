b56d328bc47852610d021b23408be35ac0ae85be
Jonathan Brown <brownjona1@ecs.vuw.ac.nz>
2018-09-30 12:07:59 +1300

    Closes #26

---
+++ Assets/Scripts/RaycastPlanes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+	This script keeps the raycast planes at a position relative to the player fish.
+	This makes the fish face the cursor consistently.
+*/
+public class RaycastPlanes : MonoBehaviour
+{
+
+	GameObject player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.Find("Fish_Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = player.transform.position;
+	}
+}
