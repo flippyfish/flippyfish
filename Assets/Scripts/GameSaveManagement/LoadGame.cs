bed9220fb9504c5cb2edd53284888a15b43eadf2
VICTOR <parkchan4@ecs.vuw.ac.nz>
2018-10-08 23:30:57 +1300

    Now we can New Game, Continue the game. changed few stuffs for GUI

---
+++ Assets/Scripts/GameSaveManagement/LoadGame.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+//using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGame : MonoBehaviour {
+    /*
+    public static LoadGame instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this);
+        }
+        DontDestroyOnLoad(this);
+    }
+    */
+    public bool isDirectory()
+    {
+        return Directory.Exists(Application.persistentDataPath + "/saved_game");
+    }
+
+    public void Load()
+    {
+        if (!isDirectory())
+        {
+            Directory.CreateDirectory(Application.persistentDataPath + "/saved_game");
+        }
+        // BinaryFormatter bf = new BinaryFormatter();
+        if (File.Exists(Application.persistentDataPath + "/saved_game/saved_game.save")) {
+            //FileStream file = File.Open(Application.persistentDataPath + "/saved_game/saved_game.save", FileMode.Open);
+            //JsonUtility.FromJsonOverwrite((string) bf.Deserialize(file), );
+            //file.Close();
+            StreamReader reader = new StreamReader(Application.persistentDataPath + "/saved_game/saved_game.save");
+            string sceneName = reader.ReadLine();
+            reader.Close();
+            SceneManager.LoadScene(sceneName);
+            Debug.Log("Loading...");
+        } 
+        Debug.Log("Failed to load saved game");
+    }
+}
---
+++ Assets/Scripts/GameSaveManagement/SaveGame.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveGame : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        save(sceneName);
+        Debug.Log("game saved at: " + Application.persistentDataPath + "/saved_game/saved_game.save");
+    }
+
+    /*
+    public static SaveGame instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this);
+        }
+        DontDestroyOnLoad(this);
+    }
+    */
+    public bool isDirectory()
+    {
+        return Directory.Exists(Application.persistentDataPath + "/saved_game");
+    }
+
+    public void save(string sceneName)
+    {
+        if (!isDirectory()) {
+            Directory.CreateDirectory(Application.persistentDataPath + "/saved_game");
+        }
+        // These are for the case of saving object, We only need to save String as a file
+        //BinaryFormatter bf = new BinaryFormatter();
+        //FileStream file = File.Create(Application.persistentDataPath + "/saved_game/saved_game.save");
+        //var json = JsonUtility.ToJson(sceneName);
+        //bf.Serialize(file, json);
+        //file.Close();
+        StreamWriter writer = new StreamWriter(Application.persistentDataPath + "/saved_game/saved_game.save", false);
+        writer.WriteLine(sceneName);
+        writer.Close();
+    }
+}
