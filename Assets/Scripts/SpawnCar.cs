af999136f24fc37f02d129f41f1f7d75f38f1929
Keegan Edwards <edwardkeeg@ecs.vuw.ac.nz>
2018-10-10 14:51:45 +1300

    Added stuff

--- Assets/Scripts/SpawnCar.cs
+++ Assets/Scripts/SpawnCar.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnHazardsLevel3 : MonoBehaviour {
+public class SpawnCar : MonoBehaviour {
 
 	public GameObject car;
 	public float carInterval;
+	public float spawnX;
+	public float spawnY;
+	public float spawnZ;
+	public float spawnRot;
 
 	// Use this for initialization
 	void Start ()
@@ -18,8 +22,8 @@ public class SpawnHazardsLevel3 : MonoBehaviour {
         while (true)
         {
             // spawn car
-            Vector3 spawnPosition = new Vector3(-40.0f, 1.644637f, 22.0f);
-            Quaternion spawnRotation = Quaternion.LookRotation(new Vector3(1.0f, 0.0f, 0.0f));
+            Vector3 spawnPosition = new Vector3(spawnX, spawnY, spawnZ);
+            Quaternion spawnRotation = Quaternion.LookRotation(new Vector3(spawnRot, 0.0f, 0.0f));
             Instantiate(car, spawnPosition, spawnRotation);
 
             // time between cars
