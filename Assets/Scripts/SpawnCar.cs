72c2425104e9e0b940e7e828d8cfd2b6458736b6
edwardkeeg <edwardkeeg@myvuw.ac.nz>
2018-10-11 13:32:00 +1300

    Added new textures in City_Lv1, expanded City_Lv2, updated SpawnCar script

--- Assets/Scripts/SpawnCar.cs
+++ Assets/Scripts/SpawnCar.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class SpawnCar : MonoBehaviour {
 
-	public GameObject car;
+	public GameObject car1;
+	public GameObject car2;
+	public GameObject car3;
 	public float carInterval;
 	public float spawnX;
 	public float spawnY;
 	public float spawnZ;
 	public float spawnRot;
+
+	private float rand;
 
 	// Use this for initialization
 	void Start ()
@@ -21,10 +25,22 @@ public class SpawnCar : MonoBehaviour {
 	{
         while (true)
         {
+        	rand = Random.Range(0.0f, 3.0f);
             // spawn car
             Vector3 spawnPosition = new Vector3(spawnX, spawnY, spawnZ);
             Quaternion spawnRotation = Quaternion.LookRotation(new Vector3(spawnRot, 0.0f, 0.0f));
-            Instantiate(car, spawnPosition, spawnRotation);
+            if(rand<1.0)
+            {
+            	Instantiate(car1, spawnPosition, spawnRotation);
+            }
+            if(rand<2.0 && rand>1.0)
+            {
+            	Instantiate(car2, spawnPosition, spawnRotation);
+            }
+            if(rand>2.0)
+            {
+            	Instantiate(car3, spawnPosition, spawnRotation);
+            }
 
             // time between cars
             yield return new WaitForSeconds(carInterval);
