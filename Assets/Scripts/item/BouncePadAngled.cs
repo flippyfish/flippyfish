ce31fae9028e876f8e9c01b6b66f01d83d44989d
Jonathan Brown <brownjona1@ecs.vuw.ac.nz>
2018-10-27 14:36:08 +1300

    Closes #78

---
+++ Assets/Scripts/item/BouncePadAngled.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePadAngled : MonoBehaviour
+{
+
+	public float charge;
+	public float angle;				// 0 to 360
+	private Quaternion rotation;
+
+	// Use this for initialization
+	void Start()
+	{
+		rotation = Quaternion.Euler(0, angle, 0);
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+
+		if (collision.gameObject.name == "Fish_Player")
+		{
+			Debug.Log("Bounce Pad, Fish on me!");
+			collision.gameObject.transform.rotation = rotation;
+			collision.gameObject.GetComponent<FishMovement>().DoJump(charge);
+		}
+	}
+}
