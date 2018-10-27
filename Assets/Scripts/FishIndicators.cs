bb49fd961fb9001cf0954c1189d4ab4b07847579
Jonathan Brown <brownjona1@ecs.vuw.ac.nz>
2018-10-27 14:59:34 +1300

    #63: Adjusted the scale of the distance indicator

--- Assets/Scripts/FishIndicators.cs
+++ Assets/Scripts/FishIndicators.cs
@@ -6,11 +6,12 @@ public class FishIndicators : MonoBehaviour
 {
 
 	private GameObject fishKinematic;		// "ghost" fish that faces the cursor while charging a jump
-	private GameObject distanceMeasure;		// measuring tape that shows the player how far the fish can go
+	private GameObject distanceMeasure;		// measuring stick that shows the player the direction the fish will go
 	private GameObject distanceBar;			// moving bar that shows how far the fish would jump
 
-	private PowerBar powerBar;
+	private PowerBar powerBar;				// other fish script
 
+	public float scale;						// calibrate how far the moving distance bar travels
 	public bool active;
 
 	// Use this for initialization
@@ -19,7 +20,7 @@ public class FishIndicators : MonoBehaviour
 		fishKinematic   = GameObject.Find("Fish_Player kinematic");
 		distanceMeasure = GameObject.Find("Distance measure");
 		distanceBar     = GameObject.Find("Distance bar");
-		//powerBar        = GetComponent<PowerBar>();	// PowerBar script is added to fish at runtime, so must get it later
+		powerBar        = GetComponent<PowerBar>();
 
 		StopIndicators();
 	}
@@ -46,57 +47,52 @@ public class FishIndicators : MonoBehaviour
 	}
 
 	/*
-     * Moves and rotates the ghost fish, the distance tape measure, and the moving distance bar.
-     *  - Jonathan
-     */
-    public void PointJumpIndicators(int layerMask)
-    {
-        // if objects are hidden, make them visible
-        if (!active)
-        {
-            StartIndicators();
-        }
+	 * Moves and rotates the ghost fish, the distance tape measure, and the moving distance bar.
+	 *  - Jonathan
+	 */
+	public void PointJumpIndicators(int layerMask)
+	{
+		// if objects are hidden, make them visible
+		if (!active)
+		{
+			StartIndicators();
+		}
 
-        if (powerBar == null)
-        {
-            powerBar = GetComponent<PowerBar>();
-        }
+		// start with ghost fish
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+		{
+			// move the kinematic fish to where the real fish is
+			fishKinematic.transform.position = new Vector3(transform.position.x, transform.position.y + 0.15f, transform.position.z);
 
-        // start with ghost fish
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
-        {
-            // move the kinematic fish to where the real fish is
-            fishKinematic.transform.position = new Vector3(transform.position.x, transform.position.y + 0.15f, transform.position.z);
+			// now make it look toward the point
+			float lookY = fishKinematic.gameObject.transform.position.y;
+			Vector3 lookAt = new Vector3(hit.point.x, lookY, hit.point.z);  // the kinematic fish will look on its own y level
+			fishKinematic.transform.LookAt(lookAt);
 
-            // now make it look toward the point
-            float lookY = fishKinematic.gameObject.transform.position.y;
-            Vector3 lookAt = new Vector3(hit.point.x, lookY, hit.point.z);  // the kinematic fish will look on its own y level
-            fishKinematic.transform.LookAt(lookAt);
-
-            // finally, rotate the fish by 90 degrees so its head faces forward
-            Quaternion rotate90 = Quaternion.Euler(0, 90, 0);
-            fishKinematic.transform.rotation *= rotate90;
+			// finally, rotate the fish by 90 degrees so its head faces forward
+			Quaternion rotate90 = Quaternion.Euler(0, 90, 0);
+			fishKinematic.transform.rotation *= rotate90;
 
 
-            // do the distance measure
-            distanceMeasure.transform.position = transform.position;
-            distanceMeasure.transform.LookAt(lookAt);
+			// do the distance measure
+			distanceMeasure.transform.position = transform.position;
+			distanceMeasure.transform.LookAt(lookAt);
 
 
-            // do the moving distance bar
-            Vector3 fishPosition = transform.position;
-            float degrees = distanceMeasure.gameObject.transform.rotation.eulerAngles.y;
-            float radians = degrees * Mathf.Deg2Rad;
-            float distance = powerBar.GetCurrentPower() * 8.0f;
+			// do the moving distance bar
+			Vector3 fishPosition = transform.position;
+			float degrees = distanceMeasure.gameObject.transform.rotation.eulerAngles.y;
+			float radians = degrees * Mathf.Deg2Rad;
+			float distance = powerBar.GetCurrentPower() * scale;
 
-            float x = fishPosition.x + Mathf.Sin(radians) * distance;
-            float y = fishPosition.y;
-            float z = fishPosition.z + Mathf.Cos(radians) * distance;
+			float x = fishPosition.x + Mathf.Sin(radians) * distance;
+			float y = fishPosition.y;
+			float z = fishPosition.z + Mathf.Cos(radians) * distance;
 
-            distanceBar.transform.position = new Vector3(x, y, z);
-            distanceBar.transform.rotation = Quaternion.Euler(0, degrees, 0);
-        }
-    }
+			distanceBar.transform.position = new Vector3(x, y, z);
+			distanceBar.transform.rotation = Quaternion.Euler(0, degrees, 0);
+		}
+	}
 }
